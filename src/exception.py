fbdef192960a77b8d2cd9a18e767e7ff29eda0d6
Ayush-8955 <ayush058955@gmail.com>
2026-02-08 11:18:53 +0530

    added data transformation part

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-from src.logger import logging
+# from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
 
@@ -16,8 +16,9 @@ def error_message_detail(error, error_detail:sys):
 
 class CustomException(Exception): #inherent parent Exception
     def __init__(self,error_message,error_detail:sys):
-        super().__init__(error_message)
+        
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
+        super().__init__(error_message)
 
 
     def __str__(self):
