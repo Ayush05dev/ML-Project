3d9d097e841a62b2609a024e069b97947dd885c8
Ayush-8955 <ayush058955@gmail.com>
2026-02-08 13:19:35 +0530

    Model Trainer part done

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
+from sklearn.metrics import r2_score
 
 from src.exception import CustomException
 
@@ -18,5 +19,30 @@ def save_object(file_path,obj):
             dill.dump(obj,file_obj)
 # dill is use to serialize and saving python object to a file.
 # Serialize means converting a python object(like model,preprocesser,function) into a byte stream that can be stored on disk
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+
+def evaluate_models(X_train,y_train,X_test,y_test,models):
+    try:
+        report={}
+
+        for i in range(len(list(models))):
+            model=list(models.values())[i]
+
+            model.fit(X_train,y_train) #to train model
+
+            y_train_pred=model.predict(X_train)
+
+            y_test_predict=model.predict(X_test)
+
+            train_model_score=r2_score(y_train,y_train_pred)
+
+            test_model_score=r2_score(y_test,y_test_predict)
+
+            report[list(models.keys())[i]]=test_model_score
+
+        return report
+    
     except Exception as e:
         raise CustomException(e,sys)
