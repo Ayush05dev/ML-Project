a9dbbbbb126cce2da17af32bcc41b6e0edcabf75
Ayush-8955 <ayush058955@gmail.com>
2026-02-11 15:58:03 +0530

    Hyperparameters tuning is done

--- src/utils.py
+++ src/utils.py
@@ -7,7 +7,7 @@ import dill
 from sklearn.metrics import r2_score
 
 from src.exception import CustomException
-
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
     try:
@@ -23,14 +23,23 @@ def save_object(file_path,obj):
         raise CustomException(e,sys)
     
 
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models,params):
     try:
         report={}
 
         for i in range(len(list(models))):
             model=list(models.values())[i]
+            param=params[list(models.keys())[i]]
 
-            model.fit(X_train,y_train) #to train model
+            # as catboostregressor breaks due to version compatible issue so using like this way
+            if model.__class__.__name__ == "CatBoostRegressor":
+                model.fit(X_train, y_train)
+            else:
+                gs = GridSearchCV(model, param, cv=3)
+                gs.fit(X_train, y_train)
+                model.set_params(**gs.best_params_)
+                model.fit(X_train, y_train)
+
 
             y_train_pred=model.predict(X_train)
 
